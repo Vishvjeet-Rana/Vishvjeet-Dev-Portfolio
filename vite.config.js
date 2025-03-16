e7504555012741ab1319663c1cebc8d929e126ba
Vishvjeet-Rana <aryanrana62366@gmail.com>
2025-03-16 13:18:02 +0530

    shifted base to ./

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Vishvjeet-Dev-Portfolio/",
+  base: "./",
   build: {
     outDir: "./docs", // More explicit relative path
     sourcemap: true,
